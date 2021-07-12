50e7f90d8521de97ae9bb094fca5ef32b34b7ec2
Davi Batista Schilling <davi.bschilling@mobilesoft.it>
2021-07-12 13:08:07 +0200

    changed defer to async on js imports - fix4

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,13 +11,13 @@ export default class MyDocument extends Document {
         <body className="dark-theme music-band">
           <Main />
           <NextScript />
-          <script type="text/javascript" src="assets/js/jquery-core.js" async></script>
-          <script type="text/javascript" src="assets/js/jquery-scripts.js" async></script>
-          <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js" async></script>
-          <script type="text/javascript" src="assets/js/jquery.magnific-popup.min.js" async></script>
-          <script type="text/javascript" src="assets/masterslider/masterslider.min.js" async></script>
-          <script type="text/javascript" src="assets/owl-carousel/owl.carousel.min.js" async></script>
-          <script type="text/javascript" src="assets/js/volcanno.include.js" async></script>
+          <script type="text/javascript" src="assets/js/jquery-core.js" async defer></script>
+          <script type="text/javascript" src="assets/js/jquery-scripts.js" async defer></script>
+          <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js" async defer></script>
+          <script type="text/javascript" src="assets/js/jquery.magnific-popup.min.js" async defer></script>
+          <script type="text/javascript" src="assets/masterslider/masterslider.min.js" async defer></script>
+          <script type="text/javascript" src="assets/owl-carousel/owl.carousel.min.js" async defer></script>
+          <script type="text/javascript" src="assets/js/volcanno.include.js" async defer></script>
         </body>
       </Html>
     )
