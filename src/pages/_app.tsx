e9e87215c2dbf18e6dee5ef15b13ec8127606b60
Davi Batista Schilling <davi.bschilling@mobilesoft.it>
2021-07-19 17:00:09 +0200

    refactor to use localstorage again

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,37 @@
-import type { AppContext, AppProps } from 'next/app'
-import App from 'next/app'
-import { parseCookies, setCookie } from 'nookies'
+import type { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
-import Layout from '../themes/Layout'
+import Layout from '../themes/Layout';
 
-export type Props = {
-  reload: string
-}
-
-type MyAppProps = AppProps & {
-  props: Props
-}
-
-function MyApp({ Component, pageProps, router, props }: MyAppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
 
   const [menuActive, setMenuActive] = useState(true)
 
   useEffect(() => {
-    if ((router.pathname === '/' || router.pathname === `${process.env.BACKEND_URL}/`) && !props.reload) {
-      setCookie(null, 'RELOAD', 'true')
+    const reload = localStorage.getItem('RELOAD')
+    if ((router.pathname === '/' || router.pathname === `${process.env.BACKEND_URL}/`) && !reload) {
+      // setCookie(null, 'RELOAD', 'true')
+      localStorage.setItem('RELOAD', 'true')
       checkReload()
     }
   }, [])
 
   useEffect(() => {
     console.log(router.pathname);
-
     if ((router.pathname === '/' || router.pathname === `${process.env.BACKEND_URL}/`)) {
       setMenuActive(true)
       checkReload()
     } else {
-      setCookie(null, 'RELOAD', 'true')
+      // setCookie(null, 'RELOAD', 'true')
+      localStorage.setItem('RELOAD', 'true')
       setMenuActive(false)
     }
   }, [router.pathname])
 
   const checkReload = () => {
-    if (props.reload === 'true') {
-      setCookie(null, 'RELOAD', 'false')
+    const reload = localStorage.getItem('RELOAD')
+    if (reload === 'true') {
+      // setCookie(null, 'RELOAD', 'false')
+      localStorage.setItem('RELOAD', 'false')
       if (process.env.NODE_ENV === 'production') {
         router.replace(`${process.env.BACKEND_URL}/`)
       } else {
@@ -52,19 +46,6 @@ function MyApp({ Component, pageProps, router, props }: MyAppProps) {
         <Component {...pageProps} />
       </Layout>
   )
-}
-
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext);
-  const cookies = parseCookies(appContext.ctx)
-
-  return {
-    ...appProps,
-    props: {
-      reload: cookies.RELOAD
-    }
-  }
 }
 
 export default MyApp;
