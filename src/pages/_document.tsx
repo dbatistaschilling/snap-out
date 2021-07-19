f51cdef7e3637006f2bafb5c280dd4f8d26949cc
Davi Batista Schilling <davi.bschilling@mobilesoft.it>
2021-07-19 17:42:49 +0200

    refactor to use localstorage again5

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,27 +3,6 @@ import { HeadComponent } from '../components'
 
 export default class MyDocument extends Document {
 
-  componentDidMount(){
-    jQuery(document).ready(function ($) {
-      'use strict';
-
-      // MASTER SLIDER START
-      VolcannoInclude.masterSliderInit("masterslider-band");
-
-      // INCLUDE LATEST PORTFOLIO CAROUSEL
-      VolcannoInclude.owlCarouselInit('latest-portfolio-carousel');
-
-      // INCLUDE LATEST POSTS CAROUSEL
-      VolcannoInclude.owlCarouselInit('latest-posts-carousel-03');
-
-      // FOOTER NEWSLETTER
-      VolcannoInclude.magnificPopupInit('newsletter-popup');
-
-      // AUDIO POPUP
-      VolcannoInclude.magnificPopupInit('audio-popup');
-  });
-  }
-
   render() {
     return (
       <Html>
